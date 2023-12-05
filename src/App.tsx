b6be781336140c8e12311c7737208f3cd24692ac
BianGode <bian2205@gmail.com>
2023-12-05 18:49:19 +0100

    Added basic loggin system and database structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { auth } from "./firebase"
+import { User, onAuthStateChanged } from 'firebase/auth'
+import firebase from 'firebase/compat/app'
 
 
 export default function App() {
-  console.log(auth.currentUser)
-  
-  if (auth.currentUser) {
-    console.log(auth.currentUser)
-  }
+  const [user, setUser] = useState<User | null>(null)
+
+  onAuthStateChanged(auth, (firebaseUser) => {
+    setUser(firebaseUser)
+  })
   return (
     <>
-      <h1>home</h1>
+      {
+        user !== null ?
+          <h1>{user.email}</h1>
+          : <h1>Not logged in</h1>
+      }
     </>
   )
 }
