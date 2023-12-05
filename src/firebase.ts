8aa1926263379a02c0c81818a038001533fcdefe
BianGode <bian2205@gmail.com>
2023-12-05 12:43:44 +0100

    firebase config setup

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD06QCb3Ye2yTR7r62fQk8WDl0rkSp69Io",
+  authDomain: "meowmeow-af606.firebaseapp.com",
+  projectId: "meowmeow-af606",
+  storageBucket: "meowmeow-af606.appspot.com",
+  messagingSenderId: "454330947147",
+  appId: "1:454330947147:web:1fe23ac8990bd655789fce"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// manage your cat via a dashboard
+// figure out how
