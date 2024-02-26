1464d1e63c6fa6c14da4e4fc5f106e59e04510d1
BianGode <bian2205@gmail.com>
2024-02-26 13:13:45 +0100

    tried some s but didn't work

--- src/UserContext.ts
+++ src/UserContext.ts
@@ -1,7 +1,5 @@
-import { createContext } from "react";
+
 export interface contextData {
   email: string;
   fun: () => void;
 }
-export const UserContext = createContext<contextData>('')
-
--- src/routes/CreateCat.tsx
+++ src/routes/CreateCat.tsx
@@ -7,18 +7,22 @@ import '../styles/createCat.css'
 import { UserContext } from "../UserContext"
 
 interface createCatProps {
-  email:string,
+  email: string,
 }
 
 export default function CreateCat(props: createCatProps) {
 
   // set the state props to local variable
   // side note = I still need to research if i have to assign it to a variable or state variable
-  const context = useContext(UserContext)
-  const email = context.email
-  const fun = context.fun
+  const { email, fun } = useContext(UserContext)
+  if (!context) {
+    throw Error("useAuthContext can only be used inside an AuthProvider");
+  } else {
+    const email = context.email
+    const fun = context.fun
+  }
 
-  if(document.querySelector('.Sidebar')?.classList.contains('on')) {
+  if (document.querySelector('.Sidebar')?.classList.contains('on')) {
     fun()
   }
 
@@ -31,7 +35,7 @@ export default function CreateCat(props: createCatProps) {
   // maybe siblings
 
   // list of all the breeds (iknow i should pull this data or put it in a seperate text file but.... fuck it)
-  const catBreeds:string[] = ["Abyssinian", "Aegean", "American Bobtail", "American Curl", "American Shorthair", "American Wirehair", "Aphrodite Giant", "Arabian Mau", "Ashera", "Asian Longhair", "Asian Shorthair", "Australian Mist", "Aztec", "Balinese", "Balinese Modern", "Bambino", "Bengal", "Birman", "Bombay", "Bramble", "Brazilian Shorthair", "British Longhair", "British Shorthair", "Burmese", "Burmilla", "Chartreux", "Chausie", "Cornish Rex", "Cyprus", "Devon Rex", "Domestic Long Hair", "Domestic Shorthair", "Dragon Li", "Egyptian Mau", "Exotic", "Himalayan", "Javanese", "Korat", "LaPerm", "Lykoi", "Maine Coon", "Manx", "Minskin", "Norwegian Forest Cat", "Ocicat", "Oriental Longhair", "Oriental Shorthair", "Persian Modern", "Persian Traditional", "Ragamuffin", "Ragdoll", "Russian Blue", "Savannah", "Scottish Fold", "Selkirk Rex", "Siamese", "Siamese Modern", "Siberian", "Singapura", "Snowshoe", "Sphynx", "Tonkinese", "Turkish Angora", "Turkish Van"]
+  const catBreeds: string[] = ["Abyssinian", "Aegean", "American Bobtail", "American Curl", "American Shorthair", "American Wirehair", "Aphrodite Giant", "Arabian Mau", "Ashera", "Asian Longhair", "Asian Shorthair", "Australian Mist", "Aztec", "Balinese", "Balinese Modern", "Bambino", "Bengal", "Birman", "Bombay", "Bramble", "Brazilian Shorthair", "British Longhair", "British Shorthair", "Burmese", "Burmilla", "Chartreux", "Chausie", "Cornish Rex", "Cyprus", "Devon Rex", "Domestic Long Hair", "Domestic Shorthair", "Dragon Li", "Egyptian Mau", "Exotic", "Himalayan", "Javanese", "Korat", "LaPerm", "Lykoi", "Maine Coon", "Manx", "Minskin", "Norwegian Forest Cat", "Ocicat", "Oriental Longhair", "Oriental Shorthair", "Persian Modern", "Persian Traditional", "Ragamuffin", "Ragdoll", "Russian Blue", "Savannah", "Scottish Fold", "Selkirk Rex", "Siamese", "Siamese Modern", "Siberian", "Singapura", "Snowshoe", "Sphynx", "Tonkinese", "Turkish Angora", "Turkish Van"]
 
 
   // function to make colors add to the colors state array and later add to firebase
@@ -39,7 +43,7 @@ export default function CreateCat(props: createCatProps) {
     let key: string = e.key;
 
     if (key == "Enter") {
-      
+
       if (colorArr.length > 0) {
         const tempColors: string[] = [...colorArr]
         tempColors.push(singleColor)
@@ -55,7 +59,7 @@ export default function CreateCat(props: createCatProps) {
   }
 
   // Must: user must have the ability to add a picture
-  
+
   return (
     <div className="createCatPage">
       <h1>Create a Cat!</h1>
@@ -63,16 +67,16 @@ export default function CreateCat(props: createCatProps) {
         <p>Name</p>
         <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
         <p>BirthDay</p>
-        <input type="date" value={birthDay} onChange={(e) => setBirthDay(e.target.value) } />
+        <input type="date" value={birthDay} onChange={(e) => setBirthDay(e.target.value)} />
         <div className="colorsDiv">
           <p>Color(Can Enter multiple)</p>
           <input type="text" value={singleColor} onChange={(e) => setSingleColor(e.target.value)} onKeyDown={(e) => handleColors(e)} />
           {
             colorArr.length > 0 ?
-            colorArr.map((clr, inx) => {
-              return <Color color={clr} index={inx}/>
-            })
-            : <p>No Colors yet</p>
+              colorArr.map((clr, inx) => {
+                return <Color color={clr} index={inx} />
+              })
+              : <p>No Colors yet</p>
           }
         </div>
         <p>Eye color</p>
@@ -80,11 +84,11 @@ export default function CreateCat(props: createCatProps) {
         <p>Type of cat </p>
         {/* maybe create a custom select component */}
         <select name="catBreeds" value={type} onChange={(e) => setType(e.target.value)}>
-        {
-          catBreeds.map((breed,idx)=> {
-            return <option key={idx} value={breed}>{breed}</option>
-          })
-        }
+          {
+            catBreeds.map((breed, idx) => {
+              return <option key={idx} value={breed}>{breed}</option>
+            })
+          }
         </select>
         <button onClick={() => handleCreateACat(email, name, birthDay, colorArr, eyeColor, type)}>Add Cat</button>
       </div>
